78d6ed4c59eb0c8942d34d129eea8759e1ef1e32
Viktor Zwinger <45140691+GedTest@users.noreply.github.com>
2021-04-12 10:26:32 +0200

    create first model

--- catalog/models.py
+++ catalog/models.py
@@ -2,8 +2,16 @@ from django.db.models import Model, IntegerField, CharField, Choices
 
 
 # Create your models here.
-#class Brick(Model):
-#    id = IntegerField(primary_key=True)
-#    name = CharField(max_length=200, verbose_name='catalog name')
-#    color = CharField(max_length=7, verbose_name='color')
-#    type = Choices(value=2, names=[{'standard', 'special'}])
+class Brick(Model):
+    """This class is meant for individual LEGO brick pieces"""
+
+    id = IntegerField(primary_key=True)
+    name = CharField(max_length=200, verbose_name='catalog name')
+    color = CharField(max_length=7, verbose_name='color')
+    type = CharField(max_length=8, choices=[(1, 'standard'), (2, 'special')])
+
+    def __str__(self):
+        return f'{self.name} [{self.id}] is {self.type} type, of {self.color} color'
+
+    class Meta:
+        ordering = ['name']
